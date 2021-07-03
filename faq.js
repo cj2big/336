57fec07046062ff1b8aaf2f5933afbc7550f1431
CJ <34591992+cj2big@users.noreply.github.com>
2021-07-03 12:49:01 -0500

    Create faq.js

---
+++ faq.js
@@ -0,0 +1,15 @@
+fetch("faq.json")
+  .then(response => response.json())
+  .then(async data => {
+    data.forEach(val => {
+      let x = document.createElement("UL");
+      x.setAttribute("class", "collection")
+      let html = `
+        <li class="collection-item searchable">${val.q}</li>
+        <li class="collection-item searchable">${val.a}</li>
+  `;
+      x.innerHTML = html;
+      console.log(x);
+      document.getElementById("faq_list").appendChild(x);
+    });
+  });
