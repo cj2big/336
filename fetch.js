91245fee03cd48d79291e8aef38285a544af197a
CJ <34591992+cj2big@users.noreply.github.com>
2021-07-03 12:49:45 -0500

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -2,7 +2,6 @@ let links = [
   {
     text: "Home",
     href: "#",
-    html: ""
   },
   {
     text: "Contacts",
@@ -10,24 +9,26 @@ let links = [
     data: "https://json.336.zone/contacts.json",
   },
   {
-    text: "Shop: Wolf Shack",
+    text: "F.A.Q.",
+    href: "#faq",
+    data: "https://json.336.zone/faq.json"
+  },
+  {
+    text: "Wolf Shack",
     href: "https://336wolfshack.square.site/s/shop",
     target: "_blank"
-  }
+  },
 ];
-async function navLinks(id) {
+function navLinks(id) {
   links.forEach(function(link) {
-    var target = "";
-    if (link.target !== undefined){
-      target = `target="${link.target}"`
+    let target;
+    if (link.target == undefined) {
+      target = "";
+    } else {
+      target = `target="${link.target}"`;
     }
     document.getElementById(
       id
     ).innerHTML += `<li><a ${target} href="${link.href}">${link.text}</a></li>`;
-
-    if (link.data !== undefined || link.data === "") {
-      return;
-    }
-    fetch(link.data);
   });
 }
