333768ff79cc43bed90e0e3e983ef7823e832d37
CJ <34591992+cj2big@users.noreply.github.com>
2021-07-10 11:19:39 -0500

    Add files via upload

--- modules.js
+++ modules.js
@@ -1,3 +1,58 @@
+hashCheck();
+window.addEventListener(
+  "hashchange",
+  function() {
+    hashCheck();
+  },
+  false
+);
+function hashCheck() {
+  console.log("checking hash");
+  var hash = location.hash.substr(1);
+  var search = false;
+  if (hash.includes("?")) {
+    var x = hash.split("?");
+    hash = x[0];
+  }
+  var pages = document.getElementsByClassName("page_section");
+  var target = "";
+  if (hash == "") {
+    target = "home";
+  }
+  for (let i = 0; i < pages.length; i++) {
+    if (hash == pages[i].id) {
+      pages[i].style.display = "";
+    } else if (hash == "") {
+      if (pages[i].id == "home") {
+        pages[i].style.display = "";
+      } else {
+        pages[i].style.display = "none";
+      }
+    } else {
+      pages[i].style.display = "none";
+    }
+  }
+  searchUrl();
+}
+function searchUrl() {
+  let hash = location.hash.substr(1);
+  let search = false;
+  if (hash.includes("?")) {
+    let x = hash.split("?");
+    hash = x[0];
+    search = x[1];
+  }
+  if (search !== false) {
+    document.getElementById(`${hash}_search`).value = search;
+    if (hash == "contacts") {
+      searchContacts();
+    } else if (hash == "faq") {
+      searchData(`${hash}_search`, `${hash}_list`, "UL");
+    } else if (hash == "files") {
+      searchTable(`${hash}_search`, `${hash}_list`, "TR");
+    }
+  }
+}
 function searchContacts() {
   // Declare variables
   var input, filter, ul, li, a, i, txtValue;
@@ -41,29 +96,18 @@ function searchData(input, target, list) {
   }
 }
 function searchTable(input, target, el) {
-  let filter, elm, x
-  input = document.getElementById(input)
+  let filter, elm, x;
+  input = document.getElementById(input);
   filter = input.value.toUpperCase();
   target = document.getElementById(target);
   elm = target.getElementsByTagName(el);
-  
-  for(let i = 1; i < elm.length; i++){
+
+  for (let i = 1; i < elm.length; i++) {
     x = elm[i].innerText || elm[i].textContent;
-    if(x.toUpperCase().indexOf(filter)> -1){
+    if (x.toUpperCase().indexOf(filter) > -1) {
       elm[i].style.display = "";
-    }else{
+    } else {
       elm[i].style.display = "none";
     }
   }
 }
-/*
-        <div class="section">
-          <input
-            placeholder="Search"
-            id="search_faq"
-            type="text"
-            onkeyup="searchData('search_faq', 'faq_list', 'UL')"
-          />
-          <label for="search_faq">Search FAQ</label>
-        </div>
-*/
