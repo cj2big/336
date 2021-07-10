88fbb88b7b96da61e49ecc1e1648127102c3fe5a
CJ <34591992+cj2big@users.noreply.github.com>
2021-07-10 17:47:16 -0500

    Add files via upload

--- modules.js
+++ modules.js
@@ -1,19 +1,27 @@
 function searchData(id, group, start) {
-  let filter, x, elm, input, target
-  input = document.getElementById(`${id}_search`)
-  console.log(input)
-  filter = input.value.toUpperCase();
-  target = document.getElementById(`${id}_list`);
-  elm = target.getElementsByTagName(`${group}`);
-  if(start == undefined){
+  let filter, x, elm, input, target;
+  try {
+    input = document.getElementById(`${id}_search`);
+    filter = input.value.toUpperCase();
+    target = document.getElementById(`${id}_list`);
+    elm = target.getElementsByTagName(`${group}`);
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+  if (start == undefined) {
     start = 0;
   }
-  for(let i = start; i < elm.length; i++){
+  for (let i = start; i < elm.length; i++) {
     x = elm[i].innerText || elm[i].textContent;
-    if(x.toUpperCase().indexOf(filter)> -1){
+    if (x.toUpperCase().indexOf(filter) > -1) {
       elm[i].style.display = "";
-    }else{
+    } else {
       elm[i].style.display = "none";
     }
   }
 }
+
+/*
+Attempted to make this fail-proof
+*/
